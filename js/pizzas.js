d28a953c925d05ac420c70ad65be24a8a4ead5c8
EspaçoMix <105557405+EspacoMix@users.noreply.github.com>
2023-01-11 11:12:36 -0300

    Add files via upload

---
+++ js/pizzas.js
@@ -0,0 +1,50 @@
+let pizzaJson = [
+    {
+        id: 1,
+        name: 'Aluguel do Espaço',
+        img: 'img/epaco_5.jpg',
+        price: [380.00, 380.00, 380.00],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: ' '
+      },
+      {
+        id: 2,
+        name: 'Pula-Pula',
+        img: 'img/pizza2.png',
+        price: [0, 0, 100.00],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: ''
+      },
+      {
+        id: 3,
+        name: 'Pis.bolinhas',
+        img: 'img/pizza3.png',
+        price: [ ,  , 100.00],
+        sizes: [
+          ' ',
+          ' ',
+          ' '
+        ],
+        description: ' '
+      },
+      {
+        id: 4,
+        name: 'Bar.Pipoca',
+        img: 'img/pizza4.png',
+        price: [ ,  , 100.00],
+        sizes: [
+          ' ',
+          ' ',
+          ' '
+        ],
+        description: ' '
+      }
+];
